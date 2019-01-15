c9f40228fc1752e897160cb5c1a3a59913504214
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-15 12:38:18 +0100

    initializing project and adds .gitignore

---
+++ _32_KnightTour/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _32_KnightTour
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
