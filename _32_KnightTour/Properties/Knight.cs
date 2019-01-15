a0125b1aa864a45e8e7a53f5f1841fe1a85aab25
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-15 17:57:11 +0100

    adds method for the actual knights tour, brute force randomized approach

--- _32_KnightTour/Properties/Knight.cs
+++ _32_KnightTour/Properties/Knight.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+
 namespace _32_KnightTour.Properties
 {
     public class Knight
@@ -18,5 +20,59 @@ namespace _32_KnightTour.Properties
             _board = new Board();
 
         }
+
+        public void Move()
+        {
+            while(!_isFullTour)
+            {
+                _curH = _rand.Next(Board.size);
+                _curV = _rand.Next(Board.size);
+
+                _moveCounter = 1;
+                _board.PopulateArray(_curH, _curV);
+                _isEndTour = false;
+
+                do
+                {
+
+                } while (!_isEndTour);
+            }
+        }
+
+        public bool IsMoving()
+        {
+            int[] movesTaken = { 0, 0, 0, 0, 0, 0, 0, 0 };
+            var allMovesUsed = false;
+
+            while (!allMovesUsed)
+            {
+                var i = _rand.Next(8);
+                if (movesTaken[i] == 0)
+                {
+                    var hMove = _curH + _board.Horizontal[i];
+                    var vMove = _curV + _board.Vertical[i];
+
+                    if (hMove >= 0 && hMove < Board.size && vMove >= 0 && vMove < Board.size && _board.ChessBoard[hMove, vMove] == Board.boardSymbol)
+                    {
+                        _curH = hMove;
+                        _curV = vMove;
+                        _board.ChessBoard[_curH, _curV] = Board.moveSymbol;
+                        _moveCounter++;
+                        return true;
+                    }
+
+                    movesTaken[i] = 1;
+
+                }
+
+                if (!movesTaken.Contains(0))
+                    allMovesUsed = true;
+            }
+
+            _tourCount++;
+            return false;
+        }
+
+
     }
 }
