88129a76c9a1cf7f35232a0080a842029557c1a7
Albert Stjärne <albert.stjarne@gmail.com>
2019-01-15 13:10:21 +0100

    adds Knight class, with random no generator for the move, adds variables for horizontal and vertical position, and variables for statistics

---
+++ _32_KnightTour/Properties/Knight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace _32_KnightTour.Properties
+{
+    public class Knight
+    {
+        private Random _rand = new Random();
+        private int _curH;
+        private int _curV;
+        private Board _board;
+        private bool _isFullTour = false;
+        private bool _isEndTour = false;
+        private static int _bestScore = 0; //stores the most number of moves made in one tour so far
+        private static int _tourCount = 0; //counts all incomplete tours
+        private int _moveCounter;
+
+        public Knight()
+        {
+            _board = new Board();
+
+        }
+    }
+}
